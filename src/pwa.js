8bd2d447970389278e3253e06d12ad4ea6e71380
danyo1399 <danyo1399@nomail.com>
2022-05-15 01:40:56 +1200

    pwa setup

---
+++ src/pwa.js
@@ -0,0 +1,35 @@
+let hasInteracted, shouldReload;
+
+function sw() {
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker.getRegistration().then(reg => {
+			reg.onupdatefound = () => {
+				const w = reg.installing;
+				w.onstatechange = () => {
+					if (w.state === 'installed' && navigator.serviceWorker.controller) {
+						shouldReload = true;
+						if (!hasInteracted) location.reload();
+					}
+				};
+			};
+		});
+	}
+}
+
+
+	addEventListener(
+		'click',
+		e => {
+			hasInteracted = true;
+			const link = e.target.closest('a');
+			if (link && shouldReload) {
+				location.href = link.href;
+				e.preventDefault();
+				return false;
+			}
+		},
+		true
+	);
+
+	if (document.readyState === 'complete') setTimeout(sw);
+	else addEventListener('load', sw);
